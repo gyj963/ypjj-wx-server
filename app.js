bcb2cdab9733576aae5a71d1129371cceb5f0bcd
gaoyingjian <gaoyingjian@maoyan.com>
2018-10-03 21:57:20 +0800

    debug

--- app.js
+++ app.js
@@ -56,7 +56,6 @@ router.post('/wx', async (ctx, next) => {
 		ctx.body = 'failed';
 		return false;
 	}
-	await next();
 	let xmlObj = await getXmlObj(ctx);
 	if(xmlObj) {
 		let resObj = `<xml><ToUserName><![CDATA[${xmlObj.FromUserName}]]></ToUserName>
