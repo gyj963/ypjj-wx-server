01c63bfa9fb759cdb22ae890ca55d5e5c2c4291c
gaoyingjian <gaoyingjian@maoyan.com>
2018-10-06 00:51:28 +0800

    mongoose test

--- server/middleware/mongoose.js
+++ server/middleware/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://172.16.109.196/wechat');
-
+let mongooseConnect = mongoose.connect('mongodb://172.16.109.196/wechat');
+mongooseConnect.on('error', err => {
+    console.log("mongooseConnect err:", err);
+})
 module.exports = async (ctx, next) => {
     ctx.db = mongoose;
     await next()
